4c7b1ef1363f15ebfc451445c161861d221c4524
Pavel Gorbunov <darnelo@inbox.ru>
2022-12-12 12:11:03 +0300

    adapted main page

--- dist/assets/js/logos.js
+++ dist/assets/js/logos.js
@@ -1,12 +1,17 @@
 
 const logos =  document.getElementById('logos');
+console.log('go')
 
 window.addEventListener('resize', function () {
-  if (window.innerWidth <= 770 && !(logos.contains('slider__logo'))) {
+  if (window.innerWidth <= 770) {
       logos.classList.add('slider__logo');
+      logos.classList.add('slick-slider');
+      logos.classList.add('slick-initialized');
       console.log('add');
   } else {
       logos.classList.remove('slider__logo');
+      logos.classList.remove('slick-slider');
+      logos.classList.remove('slick-initialized');
       console.log('remove');
   }
 });
